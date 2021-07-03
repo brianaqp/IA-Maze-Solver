66272b54474d7018b614bf9a964922e3a942b8eb
brianqp <brianqp9889@gmail.com>
2021-07-03 16:23:22 -0500

    Ya logre una iteracion y se plasman en el maze.

--- Astar_aparte.py
+++ Astar_aparte.py
@@ -60,31 +60,49 @@ class Nodo():
         self.position = position
         self.k = 0
         # Funcion evaluacion
-        self.g = 0
+        self.g = 1
         self.f = 0
         self.h = 0
         self.nivel = 0
 
-    def dar_paso(self, k, tablero, end):
-        """Da los pasos buscando el numero, A sus vecinos los denomina con un k+1"""
-        for fila in range(len(tablero)):
-            for columna in range(len(tablero[fila])):
-                if tablero[fila][columna] == k:
-                    """Revisa arriba""" # que es lo que tengo que revisar? 1.- Si se salio del borde, y si hay obstruccion
-                    if fila-1 >= 0 and tablero[fila-1][columna] != "#" and tablero[fila-1][columna] == 0 or tablero[fila-1][columna] == "X": #si mi fila-1 no se sale del index, ok; y si es diferente de #, ok.
-                        tablero[fila - 1][columna] = k+1
-                    """Revisa abajo"""
-                    if fila+1 <= len(tablero) and tablero[fila+1][columna] != "#" and tablero[fila+1][columna] == 0 or tablero[fila+1][columna] == "X": #si mi fila+1 no se sale del index, ok; y si es diferente de #, ok.
-                        tablero[fila + 1][columna] = k+1
-                    """Revisa derecha"""
-                    if columna+1 <= len(tablero[fila]) and tablero[fila][columna+1] != "#" and tablero[fila][columna+1] == 0 or tablero[fila][columna+1] == "X":
-                        tablero[fila][columna+1] = k+1
-                    """Revisa izquierda"""
-                    if columna - 1 >= 0 and tablero[fila][columna - 1] != "#" and tablero[fila][columna - 1] == 0 or tablero[fila][columna - 1] == "X":
-                        tablero[fila][columna - 1] = k + 1
-                        # ERROR, NO SOBRESCRIBIR LOS NUMEROS DIFERENTES A 1
-        #self.imprimir_tablero(tablero)
-        print("Iteracion: ", k)
+    def dar_paso_astar(self, maze):
+        """Da los pasos buscando obtaculos, espacios ya ocupados y el final"""
+        #   Posiciones actuales
+        tablero = maze.tablero
+        fila = self.position[0] # Nodo actual
+        columna = self.position[1]
+        direcciones = []
+        lista_hijos = []
+        print("Fila: ", fila, "Columna: ", columna)
+        """Revisa arriba"""  # que es lo que tengo que revisar? 1.- Si se salio del borde, y si hay obstruccion
+        if fila-1 >= 0 and tablero[fila-1][columna] != "#" and tablero[fila-1][columna] == 0 or tablero[fila-1][columna] == "X": #si mi fila-1 no se sale del index, ok; y si es diferente de #, ok.
+            direcciones.append([fila - 1, columna])
+        """Revisa abajo"""
+        if fila+1 <= len(tablero) and tablero[fila+1][columna] != "#" and tablero[fila+1][columna] == 0 or tablero[fila+1][columna] == "X": #si mi fila+1 no se sale del index, ok; y si es diferente de #, ok.
+            direcciones.append([fila + 1, columna])
+        """Revisa derecha"""
+        if columna+1 <= len(tablero[fila]) and tablero[fila][columna+1] != "#" and tablero[fila][columna+1] == 0 or tablero[fila][columna+1] == "X":
+            direcciones.append([fila, columna+1])
+        """Revisa izquierda"""
+        if columna - 1 >= 0 and tablero[fila][columna - 1] != "#" and tablero[fila][columna - 1] == 0 or tablero[fila][columna - 1] == "X":
+            direcciones.append([fila, columna-1])
+        print("Direcciones conseguidas: ", direcciones)
+        if direcciones is None:
+            return
+        for posicion in direcciones:
+            print("Direcciones del nodo actual")
+            print(posicion)
+            hijo = Nodo(None, posicion)
+            hijo.funcion_evaluacion(maze)
+            hijo.marcar_movimiento(maze.tablero)
+            lista_hijos.append(hijo)
+        return lista_hijos
+
+    def marcar_movimiento(self, tablero):
+        print("ALSJDLAKSD")
+        print(tablero[self.position[0]][self.position[1]])
+        tablero[self.position[0]][self.position[1]] = int(self.f)
+
 
     def encontrar_camino(self, tablero, end):
         contador = 1
@@ -94,21 +112,26 @@ class Nodo():
         print("Camino encontrado! -------------")
         return contador
 
-    def funcion_evaluacion(self, punto, end):
-        x_final, x_inicial = punto[0], punto[1]
-        y_final, y_inicial = end[0], end[1]
-        print(x_final, x_inicial, "\n", y_final, y_inicial)
+    def costo_aumenta(self):
+        self.g += 1
+
+    def funcion_evaluacion(self, maze):
+        end = maze.end
+        print("\nEn funcion_evaluacion...")
+        print("Punto actual: ", self.position )
+        print("Punto destino: ", end)
+        x_final, x_inicial = end[0], self.position[0]
+        y_final, y_inicial = end[1], self.position[1]
         self.h = math.sqrt((x_final - x_inicial) ** 2 + (y_final - y_inicial) ** 2)
-        self.g = 1
         self.f = self.g + self.h
-        print("Primer heuristica: ", self.f)
+        print(self.f)
 
 if __name__ == '__main__':
     maze = Maze()
     nodo = Nodo()
     maze.crear_tablero()
-    maze.start = 1,1
-    maze.end = 5,18
+    maze.start = [1, 1]
+    maze.end = [5, 18]
     maze.ambientar_tablero(maze.start, maze.end)
     maze.imprimir_tablero()
     """En el codigo anterior, se trabajaba con una variable k
@@ -116,4 +139,24 @@ if __name__ == '__main__':
     sumandoles la heuristica"""
     # Primero que nada, establecere como se movera y se ira expandiendo el Astar
     nodo.position = maze.start  # El punto inicial del nodo para partir
-    nodo.funcion_evaluacion(nodo.position, maze.end)
+    nodo.funcion_evaluacion(maze)
+    lista_abierta = []
+    lista_cerrada = []
+    # Ya lo inializamos, ahora solo trabarlo en un while
+    lista_abierta.append(nodo)
+    print(nodo)
+    process = True
+    nodo_actual = nodo
+    while process:
+        if maze.tablero[maze.end[0]][maze.end[1]] != "X":
+            print("Camino encontrado!")
+            # process = False
+            break
+        lista_hijo = nodo_actual.dar_paso_astar(maze)    # Recibi una lista de hijos en donde a cada uno se le saco la f
+        maze.imprimir_tablero()
+        break
+
+
+
+
+
